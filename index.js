c7c0d85cb98dd7def3187eb83ef09913aee8f8ee
nicoletteoliviamarr <nmarr1021@outlook.com>
2024-05-07 14:54:04 -0500

    actual star fix

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const currentPage = window.location.pathname.split("/").pop();
 
+  const currentPathname = window.location.pathname;
+
   const navItems = document.querySelectorAll("nav ul li");
 
   navItems.forEach((item) => {
-    if (currentPage == "/" && item.querySelector("a").href.includes("index")) {
-    } else if (item.querySelector("a").href.includes(currentPage)) {
+    if (
+      currentPathname == "/" &&
+      item.querySelector("a").href.includes("index")
+    ) {
       item.classList.add("active-star");
+    } else {
+      if (item.querySelector("a").href.includes(currentPage)) {
+        item.classList.add("active-star");
+      }
     }
   });
 });
